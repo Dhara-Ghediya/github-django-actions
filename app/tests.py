13cc2b4bfa79ddf3d3bf92863c56fe5216247ead
Dhara-Ghediya <131964916+Dhara-Ghediya@users.noreply.github.com>
2023-12-12 12:48:41 +0530

    add test case

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from your_app.models import UserModel  # Replace 'your_app' with the actual app name
 
-# Create your tests here.
+class UserRegistrationTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_user_registration_success(self):
+        # Define the POST data for a successful registration
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword',
+            'firstname': 'Test',
+            'lastname': 'User',
+            'address': 'Test Address',
+            'phone': '1234567890',
+            'mailid': 'test@example.com',
+        }
+
+        # Simulate a POST request to the userRegistration view
+        response = self.client.post(reverse('userRegistration'), data)
+
+        # Assert that the response is a redirect (status code 302)
+        self.assertEqual(response.status_code, 302)
+
+        # Assert that the user is registered in the database
+        self.assertTrue(UserModel.objects.filter(username='testuser').exists())

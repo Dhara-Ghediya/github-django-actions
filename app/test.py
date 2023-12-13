3ae55f5ac2af11a059fffdb0fbc17ce17495b3a9
Dhara-Ghediya <131964916+Dhara-Ghediya@users.noreply.github.com>
2023-12-13 12:06:36 +0530

    resolve error

--- app/test.py
+++ app/test.py
@@ -31,6 +31,6 @@ class UserRegistrationTest(TestCase):
 
         # Assert that a success message is in the messages framework
         messages = [m.message for m in get_messages(response.wsgi_request)]
-        self.assertIn("Register Successfully Done! Now, You have to login...", messages)
+        self.assertIn("Register Successfully! Now, You have to login...", messages)
         
         
